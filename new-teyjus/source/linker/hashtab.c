ab98dcb4d4ef2fc9ef3cfab82f4c5a2cf6daaa98
holte <holte@da2563f4-8c35-0410-a6f7-ef3e7fa91fdf>
2006-05-19 22:41:49 +0000

    Added code relavent to loading and outputting constant hashtables from the linker.

---
+++ new-teyjus/source/linker/hashtab.c
@@ -0,0 +1,136 @@
+
+//////////////////////////////////////////////////////
+//HashTab Load and Write Code
+//////////////////////////////////////////////////////
+typedef struct{
+	int location;
+	INT1 tab_size;
+	INT1 num_entries;
+	HashTabEnt* table;
+}THashTab_t;
+
+typedef struct{
+	ConstInd index;
+	int addr;
+}HashTabEnt;
+
+typedef struct{
+	int entries;
+	int dead_entries;
+	int size;
+	TX_t* entry;
+}HashTab_Vec;
+
+HashTab_Vec HashTabs;
+
+void InitTHashTabs()
+{
+	HashTabs.dead_entries=0;
+	HashTabs.entries=0;
+	HashTabs.size=128;
+	HashTabs.entry=malloc(HashTabs.size*sizeof(THashTab_t));
+	if(HashTabs.entry==NULL)
+	{
+		perror("Memory Allocation Failed");
+		exit(0);
+	}
+}
+
+int AllocateTHashTabs(int count)
+{
+	int tmp=HashTabs.entries;
+	HashTabs.entries=HashTabs.entries+count;
+	if(HashTabs.entries>HashTabs.size)
+	{
+		do{
+			HashTabs.size*=2;
+		}while(HashTabs.entries>HashTabs.size)
+		
+		HashTabs.entry=(THashTab_t*)realloc((void*)HashTabs.entry,HashTabs.size*sizeof(THashTab_t));
+		if(HashTabs.entry==NULL)
+		{
+			perror("Memory Allocation Failed");
+			exit(0);
+		}
+	}
+	return tmp;
+}
+
+LHashTab_t* AllocateLHashTabs(int count)
+{
+	LHashTab_t* tmp=(LHashTab_t*)malloc(count*sizeof(LHashTab_t));
+	if(tmp==NULL)
+	{
+		perror("Memory Allocation Failed");
+		exit(0);
+	}
+	
+	return tmp;
+}
+
+void LoadHashTabs()
+{
+	int count=CM.HashTabcount=GET1();
+	int offset=CM.HashTaboffset=AllocateTHashTabs(count);
+	//CM.HashTab=AllocateLHashTabs(count);
+	for(int i=0;i<count;i++)
+	{
+		LoadHashTab(offset+i);
+	}
+}
+
+LHashTab_t LoadHashTab(int i)
+{
+	HashTabs.entry[i].dead=0;
+	HashTabs.entry[i].tab_size=GET1();
+	int count = HashTabs.entry[i].num_entries=GET1();
+	
+	HashTabs.entry[i].table=malloc(count*sizeof(HashTabEnt));
+	if(HashTabs.entry[i].table==NULL)
+	{
+		perror("Memory Allocation Failed");
+		exit(0);
+	}
+	
+	for(int j=0;j<count;j++)
+	{
+		HashTabs.entry[i].table[j].index=GetConstInd();
+		HashTabs.entry[i].table[j].addr=GetCodeInd();
+	}
+}
+
+void WriteHashTabs()
+{
+	PUT1(HashTabs.entries-HashTabs.dead_entries);
+	for(int i=0;i<HashTabs.entries;i++)
+	{
+		WriteHashTab(i);
+	}
+}
+
+void WriteHashTab(i)
+{
+	if(HashTabs.entry[i].dead==0)
+	{
+		PUT1(HashTabs.entry[i].tab_size);
+		PUT1(HashTabs.entry[i].num_entries);
+		for(int j=0;j<HashTabs.entry[i].num_entries;j++)
+		{
+			PutConstInd(HashTabs.entry[i].table[j].index);
+			PUT4(HashTabs.entry[i].table[j].addr);
+		}
+	}
+}
+
+int GetHashTabInd()
+{
+	return CM.HashTaboffset+GET1();
+}
+
+int MergeHashTabs(int a, int b)
+{
+	HashTabs.entry[b].dead=1;
+	THashTab_t tmp;
+	tmp.num_entries=HashTabs.entry[i].num_entries+HashTabs.entry[i].num_entries;
+	//TODO
+}
--- new-teyjus/source/linker/hashtab.h
+++ new-teyjus/source/linker/hashtab.h
@@ -1,17 +1,22 @@
 #ifndef _HASHTAB_H_
 #define _HASHTAB_H_
 
-typedef struct{
-	INT1 tab_size;
-	INT1 num_entries;
-	HashTabEnt* table;
-}HashTab;
+//HashTab Header Data
+//Note: No individual representation in CM.
+/*typedef struct{
+	//Empty
+}LHashTab_t;*/
 
-typedef struct{
-	Const_ind index;
-	int code_index;
-}Hash_tab_ent;
+void InitTHashTabs();
+void LoadHashTabs();
+void WriteHashTabs();
 
-void loadHashTab();
+
+/////////////////////////////////////////
+//Utilities////////////////////////////
+/////////////////////////////////////////
+
+int GetHashTabInd();
+int MergeHashTabs(int a, int b);
 
 #endif //_HASHTAB_H_
