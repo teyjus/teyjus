90b4898d2f5a38caeb9f16f0d019f3a0beaecb71
Zach Snow <z@zachsnow.com>
2006-06-12 01:34:08 +0000

    Somehow these files didn't get added to the repository, my mistake.
    git-svn-id: https://teyjus.googlecode.com/svn/new-teyjus@39 da2563f4-8c35-0410-a6f7-ef3e7fa91fdf

---
+++ source/compiler/compile.ml
@@ -0,0 +1,49 @@
+(**********************************************************************
+*
+**********************************************************************)
+exception Exception
+open Lpyacc
+
+let typeAbbrevTable = Table.SymbolTable.empty
+
+let printPreAbsyn = ref false
+let printAbsyn = ref false
+
+(******************************************************************
+*openFile:
+*	Open a file and exit if there is an error.
+******************************************************************)
+let openFile = fun fname f ->
+	try	
+		let inchannel = f fname in
+		inchannel
+	with Sys_error(s) -> (print_endline s; exit 1)
+
+(******************************************************************
+*closeFile:
+*	Closes a file given a closing function.
+******************************************************************)
+let closeFile = fun fname f ->
+	f fname
+
+let rec compileModule = function
+	basename ->
+		(*	Parse Source	*)
+		let inchannel = (openFile (basename ^ ".mod") open_in) in
+		let _ = Errormsg.addFile (basename ^ ".mod") in
+		let _ = Lplex.setCurrentFile (basename ^ ".mod") in
+		let lexbuf = Lexing.from_channel inchannel in
+		let result = Lpyacc.parseModule Lplex.initial lexbuf in
+		(closeFile inchannel close_in;
+		result)
+
+and compileSignature = function
+	basename ->
+		let inchannel = openFile (basename ^ ".sig") open_in in
+		let _ = Errormsg.addFile (basename ^ ".sig") in
+	
+		let _ = Lplex.setCurrentFile (basename ^ ".sig") in
+		let lexbuf = Lexing.from_channel inchannel in
+		let result = Lpyacc.parseSignature Lplex.initial lexbuf in
+		(closeFile inchannel close_in;
+		result)
