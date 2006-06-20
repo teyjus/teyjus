1426a8ab1be9652274da4a3cfcf41c4e108468b4
xqi <xqi@da2563f4-8c35-0410-a6f7-ef3e7fa91fdf>
2006-06-20 19:39:02 +0000

    instruction infor table and operand types table are no longer public, and accessfunctions to these tables are added

--- new-teyjus/source/tables/instructions.c
+++ new-teyjus/source/tables/instructions.c
@@ -14,6 +14,15 @@
 /****************************************************************************/
 /*    INSTRUCTION INFORMATION TABLE                                         */
 /****************************************************************************/
+typedef struct                        //entry of the instruction info table
+{
+    char* name;
+    INSTR_InstrCategory type;
+    int   size;
+} INSTR_InstrInfoTab_;
+
+typedef INSTR_InstrInfoTab_ INSTR_InstrInfoTab[INSTR_NUM_INSTRS]; 
+
 
 INSTR_InstrInfoTab INSTR_instrInfoTable ={
 { "unify_nil",            INSTR_CAT_X,          INSTR_CAT_X_LEN},
@@ -51,10 +60,49 @@ INSTR_InstrInfoTab INSTR_instrInfoTable ={
 { "instr_end",            INSTR_CAT_X,          INSTR_CAT_X_LEN}
 };
 
+/* Accessing functions */
+INSTR_InstrCategory INSTR_instrType(int index)
+{
+    return (INSTR_instrInfoTable[index]).type;
+}
+
+char* INSTR_instrName(int index)
+{
+    return (INSTR_instrInfoTable[index]).name;
+}
+
+int   INSTR_instrSize(int index)
+{
+    return (INSTR_instrInfoTable[index]).size;
+}
+
+
 
 /****************************************************************************/
 /*    OPERAND TYPES TABLE                                                   */
 /****************************************************************************/
+/* Max number of operand that could be taken by instructions including the  */
+/* padding bytes and one to terminate the list. (machine dependent)         */
+#ifdef MACH_64
+
+#define INSTR_MAX_OPERAND     12  //temp
+
+#else  //MACH_64 (32-bit machine)
+
+#define INSTR_MAX_OPERAND     8  
+
+#endif //MACH_64
+
+
+/* this array is indexed by instruction category.  For each category,
+   INSTR_operandTypeTab contains a string of values indicating the type
+   of the operand at that position, terminated by INSTR_X.  This
+   information is useful when parsing instruction streams. */
+typedef INSTR_OperandType 
+        INSTR_OperandTypeTab[INSTR_NUM_INSTR_CATS][INSTR_MAX_OPERAND];
+
+extern INSTR_OperandTypeTab INSTR_operandTypeTable;
+
 
 #ifdef MACH_64
 
@@ -70,33 +118,33 @@ INSTR_OperandTypeTab INSTR_operandTypeTable ={
 
     [17] = //C99 feature (not needed when the table is fully filled in)
     {INSTR_R, INSTR_C, INSTR_I2, INSTR_P, INSTR_P, INSTR_X, INSTR_X, INSTR_X,
-     INSTR_X, INSTR_X, INSTR_X, INSTR_X}  //INSTR_CAT_R_C_I2_X
+     INSTR_X, INSTR_X, INSTR_X, INSTR_X},  //INSTR_CAT_R_C_I2_X
 
     [24] = 
     {INSTR_I1, INSTR_P, INSTR_P, INSTR_P, INSTR_P, INSTR_P, INSTR_P, INSTR_L,
-     INSTR_X, INSTR_X, INSTR_X, INSTR_X}  //INSTR_CAT_I1_L_X
+     INSTR_X, INSTR_X, INSTR_X, INSTR_X},  //INSTR_CAT_I1_L_X
 
     [26] = 
     {INSTR_I1, INSTR_P, INSTR_P, INSTR_P, INSTR_P, INSTR_P, INSTR_P, INSTR_HT,
-     INSTR_X, INSTR_X, INSTR_X, INSTR_X}  //INSTR_CAT_I1_HT_X
+     INSTR_X, INSTR_X, INSTR_X, INSTR_X},  //INSTR_CAT_I1_HT_X
 
     [33] = 
     {INSTR_N, INSTR_P, INSTR_P, INSTR_P, INSTR_P, INSTR_P, INSTR_P, INSTR_L,
-     INSTR_L, INSTR_X, INSTR_X, INSTR_X}  //INSTR_CAT_N_L_L_X
+     INSTR_L, INSTR_X, INSTR_X, INSTR_X},  //INSTR_CAT_N_L_L_X
     [34] = 
     {INSTR_P, INSTR_P, INSTR_P, INSTR_P, INSTR_P, INSTR_P, INSTR_P, INSTR_L,
-     INSTR_L, INSTR_L, INSTR_L, INSTR_X}  //INSTR_CAT_L_L_L_L_X
+     INSTR_L, INSTR_L, INSTR_L, INSTR_X},  //INSTR_CAT_L_L_L_L_X
 
     [45] = 
     {INSTR_I1, INSTR_N, INSTR_P, INSTR_P, INSTR_P, INSTR_P, INSTR_P, INSTR_X,
-     INSTR_X, INSTR_X, INSTR_X, INSTR_X}  //INSTR_CAT_I1_N_X
+     INSTR_X, INSTR_X, INSTR_X, INSTR_X},  //INSTR_CAT_I1_N_X
     [46] = 
     {INSTR_P, INSTR_I2, INSTR_P, INSTR_P, INSTR_P, INSTR_P, INSTR_BVT, INSTR_X,
-     INSTR_X, INSTR_X, INSTR_X, INSTR_X}  //INSTR_CAT_I2_BVT_X
+     INSTR_X, INSTR_X, INSTR_X, INSTR_X},  //INSTR_CAT_I2_BVT_X
 
     [55] = 
     {INSTR_I1, INSTR_P, INSTR_P, INSTR_P, INSTR_P, INSTR_P, INSTR_P, INSTR_L,
-     INSTR_L, INSTR_X, INSTR_X, INSTR_X}  //INSTR_CAT_I1_L_L_X
+     INSTR_L, INSTR_X, INSTR_X, INSTR_X},  //INSTR_CAT_I1_L_L_X
 };
 
 
@@ -144,6 +192,11 @@ INSTR_OperandTypeTab INSTR_operandTypeTable ={
 };
 
 #endif //MACH_64 (32-bit machines)
+
+INSTR_OperandType* INSTR_operandTypes(INSTR_InstrCategory index)
+{
+    return INSTR_operandTypeTable[index];
+}
 
 
 #endif //INSTRUCTIONS_C
