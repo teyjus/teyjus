508570b1e66028c2e448a78b1bcb390eeb23f7ad
Xiaochu Qi <xqi@cs.umn.edu>
2006-06-24 05:09:36 +0000

    Files hopu.h and hopu.c contain an interpretive higher-order pattern unification procedure.
    git-svn-id: https://teyjus.googlecode.com/svn/new-teyjus@54 da2563f4-8c35-0410-a6f7-ef3e7fa91fdf

---
+++ source/simulator/hopu.h
@@ -0,0 +1,12 @@
+/****************************************************************************/
+/*  File hopu.h. This header file defines the interface components for the  */
+/*  code in hopu.c that implements higher-order pattern unification.        */
+/*                                                                          */
+/****************************************************************************/
+
+#ifndef HOPU_H
+#define HOPU_H
+
+void HOPU_patternUnify();
+
+#endif //HOPU_H
