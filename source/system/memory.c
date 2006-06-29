d9175f6088be64d501ebeef7fa1e351a736acc52
Xiaochu Qi <xqi@cs.umn.edu>
2006-06-29 02:57:46 +0000

    top-level data objects
    git-svn-id: https://teyjus.googlecode.com/svn/new-teyjus@61 da2563f4-8c35-0410-a6f7-ef3e7fa91fdf

--- source/system/memory.c
+++ source/system/memory.c
@@ -1,87 +1,121 @@
 #ifndef MEMORY_C
 #define MEMORY_C
 
+#include "error.h"
 #include "memory.h"
-#include "../simulator/mctypes.h"        //to be changed
-#include "../simulator/dataformats.h"    //to be changed
+#include "../simulator/mctypes.h" //to be changed
+
+/*****************************************************************************/
+/*                          GLOBAL MODULE TABLE                              */
+/*****************************************************************************/
+MEM_Gmt    MEM_modTable;                 //global module table 
+
+/******************************************************************************/
+/*                SYSTEM MEMORY MANAGEMENT  (TEMP)                            */
+/******************************************************************************/
+WordPtr MEM_memBeg;       //starting addr of the system memory
+WordPtr MEM_memEnd;       //end addr of the system memory
+WordPtr MEM_memTop;       //the first usable word in the system memory
+
+/* These variables are needed when string areas are allocated at the end of   */
+/* the system memory                                                          */
+/* The "string stack" is assumed to grow from higher address to low           */
+WordPtr MEM_strAreaBeg;   //starting addr of the string area
+WordPtr MEM_strAreaEnd;   //end addr of the string area
 
 
-/**********************************************************************/
-/*      KIND SYMBOL TABLE                                             */
-/**********************************************************************/
-typedef struct          //TO BE DECIDED
+/* Asking for the system memory of a given size (in word),                    */
+/* and initialize relevant global variables.                                  */
+void   MEM_memInit(unsigned int size)
 {
-    char   *name;
-    int    arity;    
-} MEM_KstEntry;
+    MEM_memBeg = MEM_memTop = (WordPtr)EM_malloc(size * sizeof(Word));
+    MEM_memEnd = MEM_memBeg + (size - 1);
+    
+    MEM_strAreaBeg = MEM_strAreaEnd = MEM_memEnd;
+}
 
-/**********************************************************************/
-/*      CONSTANT SYMBOL TABLE                                         */
-/**********************************************************************/
-MemPtr MEM_cstBase;                     //starting addr of const symbol table
 
-// TO BE DECIDED
-typedef struct          
+/*****************************************************************************/
+/*               ACCESSING THE IMPLICATION GOAL TABLE                        */
+/*****************************************************************************/
+/* functions for filling in the fields of a impl table                       */
+//#pred field (def extended)
+void MEM_implPutNOP(WordPtr tab, Word nop)        { *tab = nop;             }
+//ptr to find code func
+void MEM_implPutFCP(WordPtr tab, Word fcPtr)      { *(tab + 1) = fcPtr;     }
+//num entries in the link tab
+void MEM_implPutTS(WordPtr tab, Word tabSize)     { *(tab + 2) = tabSize;   }
+//link tab
+void MEM_implPutLT(WordPtr tab, int ind, Word cst){ *(tab + 3 + ind) = cst; }
+//start addr of search tab
+WordPtr MEM_implMovPT(WordPtr tab, int nop)       { return (tab + 3 + nop); }
+
+
+/* functions for retrieving the fields of a impl table                       */
+//pred field (def extended)
+Mem    MEM_implNOP(MemPtr tab){ return (*tab);              }
+//ptr to find code func
+MemPtr MEM_implFCP(MemPtr tab){ return (MemPtr)(*(tab + 1));}
+//num entries in the link tab
+Mem    MEM_implTS(MemPtr tab) { return (*(tab + 2));        }
+//start add of seq. of pred (link tab)
+MemPtr MEM_implLT(MemPtr tab) { return (MemPtr)(tab + 3);   }
+//start add of search tab
+MemPtr MEM_implPT(MemPtr tab)    
 {
-    char     *name;
-    int      typeEnvSize;
-    DF_Type  typeSkel;
-    // needness vector
-    int      univCount;
-    int      precedence;
-    int      fixity;
-} MEM_CstEntry;
+    //tab+3+ MEM_implNOP(tab) ?
+    return (MEM_implLT(tab)+ (long)MEM_implNOP(tab));
+}
 
-#define MEM_CST_ENTRY_SIZE         7    //size (in word) of each entry
+/*****************************************************************************
+ *                  ACCESSING THE IMPORTED MODULE TABLE                      *
+ *****************************************************************************/
+/* functions for filling in the fields of an import table                   */
+//# code segments
+void    MEM_impPutCSEG(WordPtr tab, Word nseg)     {   *tab = nseg;          }
+//# local constants
+void    MEM_impPutNLC(WordPtr tab, Word nlc)       {   *(tab + 1) = nlc;     }
+//# pred (def extended)
+void    MEM_impPutNOP(WordPtr tab, Word nop)       {   *(tab + 2) = nop;     }
+//ptr to find code func
+void    MEM_impPutFCP(WordPtr tab, Word fcp)       {   *(tab + 3) = fcp;     } 
+//# entries in link tab
+void    MEM_impPutTS(WordPtr tab, Word tabSize)    {   *(tab + 4) = tabSize; }
+//link tab
+void    MEM_impPutLT(WordPtr tab, int ind, Word pred){ *(tab+5+ind) = pred;  }
+//start addr of local c tab    
+//Note the tab addr is calculated from the start of the imp tab
+WordPtr MEM_impMovToLCT(WordPtr tab, int nop)      {  return (tab + 5 + nop);}  
+//loc c tab(may null)
+//Note the input tab addr should be the starting addr of the local const tab
+void    MEM_impPutLCT(WordPtr lcTab, int ind, Word cst){ *(lcTab+ind) = cst; }
+//start addr of sch tab
+//Note the tab addr is calculated from the start of the imp tab
+WordPtr MEM_impMovToPT(WordPtr tab, int nloc, int nop){return (tab+5+nloc+nop);}
 
 
-MEM_CstEntry *MEM_cstCurrent;           //current cst top
-
-void MEM_cstInit(MemPtr base)
+/* functions for retrieving the fields of a impl table                       */
+//# code segments
+Mem    MEM_impCSEG(MemPtr tab)   {  return *tab;               }
+//# local constants 
+Mem    MEM_impNLC(MemPtr tab)    {  return *(tab+1);           }  
+//# of preds (def extended)
+Mem    MEM_impNOP(MemPtr tab)    {  return *(tab+2);           }  
+//ptr to find code func
+MemPtr MEM_impFCP(MemPtr tab)    {  return (MemPtr)(*(tab+3)); } 
+//# entries in pred name tab 
+Mem    MEM_impTS(MemPtr tab)     {  return *(tab+4);           }  
+//start addr of seq. of pred (pred name tab) 
+MemPtr MEM_impLT(MemPtr tab)     {  return (MemPtr)(*(tab+5)); } 
+//start addr of local const tab (possible null)
+MemPtr MEM_impLCT(MemPtr tab) 
 {
-    MEM_cstBase    = base;
-    MEM_cstCurrent = (MEM_CstEntry*)(base);
+    return (MEM_impLT(tab) + (long)MEM_impNOP(tab));//tab + 5 + MEM_impNOP(tab)?
 }
-
-void MEM_cstSimInit(MemPtr base)
+//start addr of search tab
+MemPtr MEM_impPT(MemPtr tab) 
 {
-    MEM_cstBase = base;
+    return (MEM_impLCT(tab) + (long)MEM_impNLC(tab));
 }
-
-
-void MEM_cstInsert(char* name, int typeEnvSize, DF_Type typeSkel, 
-                   int uc, int pred, int fixity)
-{
-    MEM_cstCurrent -> name = name;
-    MEM_cstCurrent -> typeEnvSize = typeEnvSize;
-    MEM_cstCurrent -> typeSkel = typeSkel;
-    MEM_cstCurrent -> univCount = uc;
-    MEM_cstCurrent -> precedence = pred;
-    MEM_cstCurrent -> fixity = fixity;
-
-    MEM_cstCurrent=(MEM_CstEntry*)(((MemPtr)MEM_cstCurrent)+MEM_CST_ENTRY_SIZE);
-}
-
-char*   MEM_cstName(int n){             //name of the const in the nth entry
-    return ((MEM_CstEntry*)(MEM_cstBase+(n*MEM_CST_ENTRY_SIZE))) -> name;
-}
-
-int     MEM_cstTyEnvSize(int n){        //type env size in the nth entry
-    return ((MEM_CstEntry*)(MEM_cstBase+(n*MEM_CST_ENTRY_SIZE))) -> typeEnvSize;
-}
-
-int     MEM_cstUnivCount(int n){        //universe count in the nth entry
-    return ((MEM_CstEntry*)(MEM_cstBase+(n*MEM_CST_ENTRY_SIZE))) -> univCount;
-}
-int     MEM_cstPred(int n){             //precedence of the const in nth entry
-    return ((MEM_CstEntry*)(MEM_cstBase+(n*MEM_CST_ENTRY_SIZE))) -> precedence;
-}
-int     MEM_cstFixity(int n){           //fixity of the const in nth entry
-    return ((MEM_CstEntry*)(MEM_cstBase+(n*MEM_CST_ENTRY_SIZE))) -> fixity;
-}
-DF_Type MEM_cstTySkel(int n){           //type skeleton of const in nth entry
-    return ((MEM_CstEntry*)(MEM_cstBase+(n*MEM_CST_ENTRY_SIZE))) -> typeSkel;
-}
-
 
 #endif  //MEMORY_C
--- source/system/memory.h
+++ source/system/memory.h
@@ -1,46 +1,176 @@
+/****************************************************************************/
+/*                                                                          */
+/*                                                                          */
+/*   File  memory.h. This header file defines the structures that are used  */
+/*   by the simulator, the loader and the top-level interation system.      */
+/*                                                                          */
+/****************************************************************************/
+
 #ifndef MEMORY_H
 #define MEMORY_H
 
 #include <limits.h>
+#include <math.h>
 #include "../simulator/mctypes.h"        //to be changed
 #include "../simulator/dataformats.h"    //to be changed
+//#include "../config.h"
 
-/**********************************************************************/
-/*      KIND SYMBOL TABLE                                             */
-/**********************************************************************/
-#define MEM_KST_MAX_IND        USHRT_MAX //TwoBytes DF_KstTabInd (dataformats.c)
+/******************************************************************************/
+/*                SYSTEM MEMORY MANAGEMENT  (TEMP)                            */
+/******************************************************************************/
+extern  WordPtr MEM_memBeg;       //starting addr of the system memory
+extern  WordPtr MEM_memEnd;       //end addr of the system memory
+extern  WordPtr MEM_memTop;       //the first usable word in the system memory
 
+/* These variables are needed when string areas are allocated at the end of   */
+/* the system memory                                                          */
+/* The "string stack" is assumed to grow from higher address to low           */
+extern  WordPtr MEM_strAreaBeg;   //starting addr of the string area
+extern  WordPtr MEM_strAreaEnd;   //end addr of the string area
 
+/* Asking for the system memory of a given size (in word),                    */
+/* and initialize relevant global variables.                                  */
+void    MEM_memInit(unsigned int size);
 
+/*****************************************************************************/
+/*                          KIND SYMBOL TABLE                                */
+/*****************************************************************************/
+/*  kind symbol table entry                                                  */
+typedef struct                                       
+{
+    char      *name;
+    TwoBytes  arity; //agree with DF_StrTypeArity (simulator/dataformats.c)
+} MEM_KstEnt;
 
+typedef MEM_KstEnt *MEM_KstPtr;
 
+/*  max possible index of kind table                                        */
+/*  (agree with DF_KstTabInd in simulator/dataformats.c)                    */ 
+#define MEM_KST_MAX_IND        USHRT_MAX
 
-/**********************************************************************/
-/*      CONSTANT SYMBOL TABLE                                         */
-/**********************************************************************/
-#define MEM_CST_MAX_IND     USHRT_MAX //TwoBytes DF_CstTabInd (dataformats.c)
+/*  size of each entry of this table (in word)                              */
+//Note this arithematic should in reality go into "config.h"
+#define MEM_KST_ENTRY_SIZE     (int)ceil(sizeof(MEM_KstEnt)/WORD_SIZE)
 
-extern MemPtr MEM_cstBase;            //starting address of count sym tab
+/*****************************************************************************/
+/*                         TYPE SKELETON TABLE                               */
+/*****************************************************************************/
+/* type skeleton table entry                                                 */
+typedef DF_TypePtr MEM_TstEnt;
 
-void  MEM_cstInit(MemPtr base);           //initialize const symbol table
-void  MEM_cstSimInit(MemPtr base);        //initialize cst for simulator
+typedef MEM_TstEnt *MEM_TstPtr;
 
+/*  max possible index of type skeleton table                                */
+#define MEM_TST_MAX_IND        USHRT_MAX 
+
+/*  size of each entry of this table (in word)                               */
+//Note this arithematic should in reality go into "config.h"
+#define MEM_TST_ENTRY_SIZE    (int)ceil(sizeof(MEM_TstEnt)/WORD_SIZE)
+
+/*****************************************************************************/
+/*                        CONSTANT SYMBOL TABLE                              */
+/*****************************************************************************/
+/*  constant symbol table entry                                              */ 
+typedef struct
+{
+    char      *name;
+    TwoBytes  typeEnvSize;
+    TwoBytes  tskTabIndex;     //index to the type skeleton table 
+    //word      neednessVec;   //needness vector (predicate constant)
+    TwoBytes  univCount;
+    int       precedence;
+    int       fixity;
+} MEM_CstEnt;
+
+typedef MEM_CstEnt *MEM_CstPtr;
+
+/*  max possible index of constant symbol table                             */
+/*  (agree with DF_CstTabInd in simulator/dataformats.c)                    */
+#define MEM_CST_MAX_IND     USHRT_MAX
                                           //add one entry at the current top
-void  MEM_cstInsert(char* name, int teSize, DF_Type tySk, int uc, int pred, 
-                    int fixity);
-
-char*   MEM_cstName(int n);              //name of the const in the nth entry 
-int     MEM_cstTyEnvSize(int n);         //type env size in the nth entry
-int     MEM_cstUnivCount(int n);         //universe count in the nth entry
-int     MEM_cstPred(int n);              //precedence of the const in nth entry
-int     MEM_cstFixity(int n);            //fixity of the const in nth entry
-DF_Type MEM_cstTySkel(int n);            //type skeleton of const in nth entry
+/*  size of each entry of this table (in word)                               */
+//Note this arithematic should in reality go into "config.h"
+#define MEM_CST_ENTRY_SIZE    (int)(sizeof(MEM_CstEnt)/WORD_SIZE)
 
 
+/*****************************************************************************/
+/*                          GLOBAL MODULE TABLE                              */
+/*****************************************************************************/
+typedef struct
+{
+    char        *modname;        //(top-level) module name 
+    CSpacePtr   addtable;        //addr to the add code table of the top module
+    MEM_KstPtr  kstBase;         //starting addr of kind table
+    MEM_TstPtr  tstBase;         //starting addr of type skel table
+    MEM_CstPtr  cstBase;         //starting addr of constant table
+    WordPtr     modSpaceBeg;     //starting addr of module space
+    WordPtr     modSpaceEnd;     //ending addr of module space
+    //(The last two fields are needed when string area is allocated at the 
+    // end of the system memory) 
+    WordPtr     *strSpaceBeg;    //starting addr of string area
+    WordPtr     *strSpaceEnd;    //end addr of string area
+} MEM_GmtEnt;
 
-/*********************************************************************/
-/*      STREAM SYMBOL TABLE                                          */
-/*********************************************************************/
+#define MEM_MAX_MODULES    255   //max number of modules (temp)
+
+typedef MEM_GmtEnt MEM_Gmt[MEM_MAX_MODULES];
+
+extern  MEM_Gmt    MEM_modTable; //global module table 
+
+
+/*****************************************************************************/
+/*               ACCESSING THE IMPLICATION GOAL TABLE                        */
+/*****************************************************************************/
+/* functions for filling in the fields of an impl table                      */
+void    MEM_implPutNOP(WordPtr tab, Word nop);        //# pred (def extended)
+void    MEM_implPutFCP(WordPtr tab, Word fcPtr);      //ptr to find code func
+void    MEM_implPutTS(WordPtr tab, Word tabSize);     //# entries link tab
+void    MEM_implPutLT(WordPtr tab, int ind, Word cst);//link tab
+//Note the tab addr is calculated from the start of the impl tab
+WordPtr MEM_implMovToPT(WordPtr tab, int nop);        //start addr of search tab
+
+/* functions for retrieving the fields of a impl table                       */
+Mem    MEM_implNOP(MemPtr tab);  //pred field (def extended)
+MemPtr MEM_implFCP(MemPtr tab);  //ptr to find code func
+Mem    MEM_implTS(MemPtr tab);   //num entries in link tab
+MemPtr MEM_implLT(MemPtr tab);   //start add of seq. of pred (link tab)
+MemPtr MEM_implPT(MemPtr tab);   //start add of search tab
+
+
+
+/*****************************************************************************
+ *                  ACCESSING THE IMPORTED MODULE TABLE                      *
+ *****************************************************************************/
+/* functions for filling in the fields of an import table                   */
+void    MEM_impPutCSEG(WordPtr tab, Word nseg); //# code segments
+void    MEM_impPutNLC(WordPtr tab, Word nlc);   //# local constants
+void    MEM_impPutNOP(WordPtr tab, Word nop);   //# pred (def extended)
+void    MEM_impPutFCP(WordPtr tab, Word fcp);   //ptr to find code func
+void    MEM_impPutTS(WordPtr tab, Word tabSize);//# entries in link tab
+void    MEM_impPutLT(WordPtr tab, int ind, Word pred);  //link tab
+//Note the input tab addr should be the starting addr of the local const tab
+void    MEM_impPutLCT(WordPtr lcTab, int ind, Word cst);//loc c tab(may null)
+//Note the tab addr is calculated from the start of the imp tab
+WordPtr MEM_impMovToLCT(WordPtr tab, int nop);  //start addr of local c tab
+//Note the tab addr is calculated from the start of the imp tab
+WordPtr MEM_impMovToPT(WordPtr tab, int nloc, int nop);//start addr of sch tab 
+
+
+/* functions for retrieving the fields of a impl table                       */
+Mem    MEM_impCSEG(MemPtr tab); //# code segments
+Mem    MEM_impNLC(MemPtr tab);  //# local constants
+Mem    MEM_impNOP(MemPtr tab);  //# of preds (def extended)
+MemPtr MEM_impFCP(MemPtr tab);  //ptr to find code func
+Mem    MEM_impTS(MemPtr tab);   //# entries in link tab
+MemPtr MEM_impLT(MemPtr tab);   //start addr of seq. of pred (link tab)
+MemPtr MEM_impLCT(MemPtr tab);  //start addr of local const tab (possible null)
+MemPtr MEM_impPT(MemPtr tab);   //start addr of search tab
+
+
+/*****************************************************************************
+ *                      functions for finding code                           *
+ *****************************************************************************/
+
 
 
 #endif  //MEMORY_H
