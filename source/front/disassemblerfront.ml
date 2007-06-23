49ab66645f08803bb39ef8aaa87e93b1bff0fb1d
Xiaochu Qi <xqi@cs.umn.edu>
2007-06-23 08:53:50 +0000

    Files are added in front for system front.
    git-svn-id: https://teyjus.googlecode.com/svn/new-teyjus@252 da2563f4-8c35-0410-a6f7-ef3e7fa91fdf

---
+++ source/front/disassemblerfront.ml
@@ -0,0 +1,16 @@
+let main () =
+  if (Parseargs.parseArgs Parseargs.Tjdis) then
+	let tablesOnly = !Parseargs.tablesOnly in
+	let instrOnly = !Parseargs.instrOnly in
+	if (tablesOnly && instrOnly) then
+	  (print_endline "Error: tables only (-t/--table) and instrunctions only (-i/--instr) can not be simultaneously selected.";
+	   1)
+	else
+	  (Bytecode.setWordSize ();
+	   Pervasiveutils.pervasiveKindIndexMappingInit ();
+	   Pervasiveutils.pervasiveConstantIndexMappingInit ();
+	   Disassembly.disassemble (!Parseargs.inputName) tablesOnly instrOnly)
+  else 1 
+
+(* execute main *)
+let _ = main ()
