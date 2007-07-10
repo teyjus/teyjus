a42b3db449a3c0e27778f9f93810da19c83bdd92
Andrew Gacek <andrew.gacek@gmail.com>
2007-07-10 19:26:58 +0000

    Xiaochu's changes to parse.ml have changed the type of translateTerm. This change updates the parse.mli file to use this new type.
    git-svn-id: https://teyjus.googlecode.com/svn/new-teyjus@277 da2563f4-8c35-0410-a6f7-ef3e7fa91fdf

--- source/compiler/parse.mli
+++ source/compiler/parse.mli
@@ -8,7 +8,9 @@ type ptnewconstant = symbol -> Absyn.aconstant Table.SymbolTable.t ->
 type ptnewkind = symbol -> int -> pos -> Absyn.akind Table.SymbolTable.t -> 
                                        Absyn.akind Table.SymbolTable.t
 
-val translateTerm : Preabsyn.pterm -> Absyn.amodule -> Absyn.aterm
+val translateTerm : Preabsyn.pterm -> Absyn.amodule ->
+  Absyn.aterm * Types.typemolecule * Absyn.atypesymbol list * Absyn.atype list
+    
 val translateClause : Preabsyn.pterm -> Absyn.amodule -> 
                                     ptnewconstant -> ptnewkind -> Absyn.aterm
 
