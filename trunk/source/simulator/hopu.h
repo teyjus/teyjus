e1ea2a6ef846543bcebbc5fb1fda8783373535bb
qixx0019 <qixx0019@da2563f4-8c35-0410-a6f7-ef3e7fa91fdf>
2007-07-26 09:28:11 +0000

    Add a readme file describing the usage of files in the simulator directory. Add some comments to types.c hopu.h siminit.h{c}.

--- trunk/source/simulator/hopu.h
+++ trunk/source/simulator/hopu.h
@@ -9,24 +9,57 @@
 #include "mctypes.h"
 #include "dataformats.h"
 
+/* A flag denoting whether new structure is created during the process of  */
+/* finding substitutions.                                                  */
 extern Boolean HOPU_copyFlagGlb;
 
+/* Return the dereference of the abstraction body of the given term.       */
 DF_TermPtr HOPU_lamBody(DF_TermPtr tPtr);
 
+/* Globalize a rigid term and make a variable binding.                     */
+/* If the term pointer to the rigid term is not one referring to a heap    */
+/* address, its atomic content is then copied into the variable to be bound*/
+/* Otherwise, the variable is made a reference to the rigid term.          */
 void       HOPU_globalizeCopyRigid(DF_TermPtr rPtr, DF_TermPtr vPtr);
+
+
+/* Globalize a flex term.                                                   */
+/* If the term pointer is one referring to a stack address, (in which case  */
+/* the flex term must be a free variable itself), the atomic content is     */
+/* copied onto the current top of heap; the free variable on stack is then  */
+/* bound to the new heap term, and the binding is trailed if necessary; the */
+/* term pointer is updated to the new heap term.                            */ 
 DF_TermPtr HOPU_globalizeFlex(DF_TermPtr fPtr);
 
-
+/* Try to find the (partial) structure of the substitution for a flex head  */
+/* of a LLambda term corresponding to an internal flex term which is not    */
+/* known to be LLambda in the compiled form of pattern unification.         */
 DF_TermPtr HOPU_flexNestedSubstC(DF_TermPtr fhPtr, DF_TermPtr args, int nargs,
                                  DF_TermPtr tmPtr, int emblev);
+
+/* Try to find the (partial) binding of the head of a flex term when        */
+/* unifying it with a rigid term possible under abstractions in the compiled*/
+/* form of pattern unification.                                             */
 DF_TermPtr HOPU_rigNestedSubstC(DF_TermPtr rhPtr, DF_TermPtr rPtr, 
                                 DF_TermPtr args, int rnargs, int emblev);
 
+
+/* Interpretively pattern unify first the pairs delayed on the PDL, then    */
+/* those delayed on the live list, if binding occured during the first step */
+/* or previous compiled unification process.                                */
+/* Upon successful termination, PDL should be empty and pairs left on the   */
+/* live list should be those other than LLambda.                            */
 void HOPU_patternUnify();
-void HOPU_patternUnifyPair();
 
-
-
+/* Interpretively pattern unify a pair of terms given as parameters. This is*/
+/* the counter part of HOPU_patterUnifyPDL that is invoked from the compiled*/
+/* part of unification. In this situation, the procedure has to be applied  */
+/* to two terms as opposed to pairs delayed on the PDL stack.               */
+/*                                                                          */
+/* The input term pointers may dereference to register and stack addresses  */
+/* Care must be taken to avoid making a reference to a register (stack)     */
+/* address in binding a variable, and in making a disagreement pair.        */
+void HOPU_patternUnifyPair(DF_TermPtr tPtr1, DF_TermPtr tPtr2);
 
 
 #endif //HOPU_H
