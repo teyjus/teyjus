acbe8afbc1b6dd1a776da31abb074cbf112ff30d
Andrew Gacek <andrew.gacek@gmail.com>
2008-03-19 05:28:14 +0000

    The UI for tjlink was written using argp, a GNU library which is not part of C standard. I had difficulty trying to provide a replacement, so I rewrote the UI in vanilla C. I dropped some command line arguments that I felt were not neccessary.
    git-svn-id: https://teyjus.googlecode.com/svn/trunk@564 da2563f4-8c35-0410-a6f7-ef3e7fa91fdf

--- source/linker/main.c
+++ source/linker/main.c
@@ -1,73 +1,90 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <argp.h>
 #include "module.h"
 #include "file.h"
 #include "message.h"
 
 int verbosity = 0;
-char* top_modname=NULL;
+char* top_modname = NULL;
 
-const char *argp_program_version = "tjlink 1.0";
-const char *argp_program_bug_address = "<SHolte@gmail.com>";
+const char *version_string = "tjlink 1.0";
 
-static char doc[] =
-    "The teyjus linker - combines compiled lambda-prolog modules into a single program";
+const char *help_string =
+"Usage: tjlink [OPTION...] MODULENAME\n"
+"The teyjus linker - combines compiled lambda-prolog modules into a single\n"
+"program\n"
+"\n"
+"  -q, --quiet                Don't produce any output\n"
+"  -v, --verbose              Produce verbose output\n"
+"  -h, --help                 Give this help list\n"
+"  -V, --version              Print program version\n"
+"\n"
+"Mandatory or optional arguments to long options are also mandatory or optional\n"
+"for any corresponding short options.\n"
+"\n"
+  "Report bugs to <SHolte@gmail.com>.";
 
-static char args_doc[] = "MODULENAME";
+const char *usage_string = 
+"Usage: tjlink [OPTION...] MODULENAME\n"
+  "Try `tjlink --help' for more information.";
 
-static struct argp_option options[] = {
-  {"verbose",  'v', 0,      0,  "Produce verbose output" },
-  {"quiet",    'q', 0,      0,  "Don't produce any output" },
-  {"silent",   's', 0,      OPTION_ALIAS },
-  {"input",    'i', ".ext", 0,  "Extensions on bytecode files"},
-  {"output",   'o', ".ext", 0,  "Extensions on linkcode files" },
-  { 0 }
-};
-
-static error_t parse_opt (int key, char *arg, struct argp_state *state)
+void help()
 {
-       /* Get the input argument from argp_parse, which we
-  know is a pointer to our arguments structure. */
-  //struct arguments *arguments = state->input;
-     
-  switch (key)
-  {
-    case 'q': case 's':
-      verbosity--;
-      break;
-    case 'v':
-      verbosity++;
-      break;
-    case 'i':
-      LK_FILE_ByteCodeExt = arg;
-      break;
-    case 'o':
-      LK_FILE_LinkCodeExt = arg;
-      break;
-     
-    case ARGP_KEY_ARG:
-      if (state->arg_num >= 1)
-        argp_usage (state);
-      top_modname = arg;
-      break;
-     
-    case ARGP_KEY_END:
-      if (state->arg_num < 1)
-        argp_usage (state);
-      break;
-     
-    default:
-      return ARGP_ERR_UNKNOWN;
-  }
-  return 0;
+  printf("%s", help_string);
+  exit(0);
 }
-     
-static struct argp argp = { options, parse_opt, args_doc, doc };
+
+void usage(int code)
+{
+  printf("%s", usage_string);
+  exit(code);
+}
+
+void version()
+{
+  printf("%s", version_string);
+  exit(0);
+}
+  
+void parse_args(int argc, char *argv[])
+{
+  int i;
+
+  for(i = 1; i < argc; i++)
+  {
+    if (!strcmp(argv[i], "-v") || !strcmp(argv[i], "--verbose"))
+      verbosity++;
+    else if (!strcmp(argv[i], "-q") || !strcmp(argv[i], "--quiet"))
+      verbosity--;
+    else if (!strcmp(argv[i], "-h") || !strcmp(argv[i], "--help"))
+      help();
+   else if (!strcmp(argv[i], "-V") || !strcmp(argv[i], "--version"))
+      version();
+    else if (argv[i][0] == '\0' || argv[i][0] == '-')
+      usage(0);
+    else
+    {
+      if (top_modname == NULL)
+        top_modname = argv[i];
+      else
+      {
+        printf("Error: More than one module file given.\n");
+        usage(1);
+      }
+    }
+  }
+
+  if (top_modname == NULL)
+  {
+    printf("Error: No module file given.\n");
+    usage(1);
+  }
+}
+    
 
 int main(int argc, char* argv[])
 {
-  argp_parse (&argp, argc, argv, 0, 0, 0);
+  parse_args(argc, argv);
   
   EM_TRY
   {
