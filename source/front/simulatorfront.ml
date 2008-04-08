e5fb1e68672bedc098c4fbd183f69b2f7d20391d
Gopalan Nadathur <gopalan@cs.umn.edu>
2008-04-08 18:20:02 +0000

    The welcome banner did not have spaces added in the previous line to compensate for the removal of spaces from the succeeding line whenever a \ was deleted. Someone needs to check this carefully at other places where the change was carried out.
    git-svn-id: https://teyjus.googlecode.com/svn/trunk@649 da2563f4-8c35-0410-a6f7-ef3e7fa91fdf

--- source/front/simulatorfront.ml
+++ source/front/simulatorfront.ml
@@ -147,12 +147,12 @@ let solveQueries () =
 
   let print_banner () =
        print_endline "Welcome to Teyjus";
-       print_endline "Copyright (C) 2008 A. Gacek, S. Holte,\
+       print_endline "Copyright (C) 2008 A. Gacek, S. Holte, \
                       G. Nadathur, X. Qi, Z. Snow";
        print_endline "Teyjus comes with ABSOLUTELY NO WARRANTY";
-       print_endline "This is free software, and you are\
+       print_endline "This is free software, and you are \
                       welcome to redistribute it";
-       print_endline "under certain conditions.\
+       print_endline "under certain conditions. \
                       Please view the accompanying file";
        print_endline "COPYING for more information"
 
