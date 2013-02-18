bca4d49bafeadb0a53d3d9a0b51cea0be32e3962
fafounet <fafounet@da2563f4-8c35-0410-a6f7-ef3e7fa91fdf>
2013-02-18 11:07:57 +0000

    Ongoing changes

--- branches/ocaml-builtins/source/front/ocaml_wrap.h
+++ branches/ocaml-builtins/source/front/ocaml_wrap.h
@@ -20,3 +20,9 @@
 
 void ocaml_simulator_main();
 int ocaml_read_term(char *str);
+
+/* Opens the file given as an argument and returns a unique identifier for this
+ * file that the functions input, input_line, lookahead, eof, readterm should
+ * give back to OCaml when they want to perform an operation on this file */
+int ocaml_open_in(char *str);
+
