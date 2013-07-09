8dfc030762ccb6b72427389f82370945b85fdee6
fafounet@gmail.com <fafounet@gmail.com@da2563f4-8c35-0410-a6f7-ef3e7fa91fdf>
2013-07-09 13:19:25 +0000

    Code simplification and more comments for explicify

--- branches/explicit-clauses/source/compiler/explicify.mli
+++ branches/explicit-clauses/source/compiler/explicify.mli
@@ -19,19 +19,26 @@
 * You should have received a copy of the GNU General Public License
 * along with Teyjus.  If not, see <http://www.gnu.org/licenses/>.
 ****************************************************************************)
-(**********************************************************************
-*Explicify:
-**********************************************************************)
 
 
-(**********************************************************************
-*explicify:
-* Given a term, explicify it 
-**********************************************************************)
+(* explicify term add_sing top_level :
+ * Given an aterm term, returns the term with every lambda prolog connective 
+ * explicit i.e. there are no more :- , pi =>  
+ * The result depends on:
+ * - add_sing indicating if the resulting aterm should be a 
+ * single element of a list (false for the top call in parsefront)
+ * - top_level indicating if the aterm is at the top level 
+ *  (true for the top call in parsefront, false in all the other cases)
+ *  This helps to determine if Pervasive.implconstant should be read as
+ *  :- or =>  as well as if a given predicate should be embedded in a list *)
 val explicify_term : Absyn.aterm -> bool -> bool ->  Absyn.aterm 
 
+(* Add introduced constants and modify defined types in the .sig file 
+ * in the following way: * every "o" which is not in the target is replaced
+ * by "list o" *)                                                       
 val add_constants : Absyn.amodule -> Absyn.amodule
 
+(* The interpreter for explicit clauses *)
 val interpreter_mod : string
 val interpreter_sig : string
 
