c9f5ca335c068d1b103bcf04b887e3edba6a26d8
Mary Southern <marys@cs.umn.edu>
2016-10-26 16:43:38 -0500

    Adding fix to strictness check.

--- source/twelf/strictness.ml
+++ source/twelf/strictness.ml
@@ -3,10 +3,20 @@
 (** Check that the argument list contains only unique bound 
     variables. *)
 let rec check_args tmlist bndrlist =
+  let same_name id1 id2 = (Lfabsyn.get_id_name id1) = (Lfabsyn.get_id_name id2) in
+  let match_term tm1 tm2 = 
+     match tm1, tm2 with
+         Lfabsyn.IdTerm(id1,_), Lfabsyn.IdTerm(id2,_)
+       | Lfabsyn.IdTerm(id1,_), Lfabsyn.AppTerm(id2,[],_)
+       | Lfabsyn.AppTerm(id1,[],_), Lfabsyn.IdTerm(id2,_)
+       | Lfabsyn.AppTerm(id1,[],_), Lfabsyn.AppTerm(id2,[],_) -> same_name id1 id2
+       | _ -> false
+  in
   match tmlist with
       [] -> true
-    | ((Lfabsyn.IdTerm(id,_) as tm) :: tms) -> 
-         (not (List.mem tm tms)) && (List.mem id bndrlist)
+    | ((Lfabsyn.IdTerm(id,_) as tm) :: tms)
+    | ((Lfabsyn.AppTerm(id,[],_) as tm) :: tms) -> 
+         (not (List.exists (match_term tm) tms)) && (List.exists (same_name id) bndrlist)
                                  && (check_args tms bndrlist)
     | _ -> false
 
@@ -19,20 +29,18 @@ let rec appears_strict_ty id ty bndrs =
         (appears_strict_ty id body (name :: bndrs))
     | Lfabsyn.AppType(name, tms,_) ->
         let check =
-          (match (name, id) with
-              (Lfabsyn.Var(v1,_,_), Lfabsyn.Var(v2,_,_))  
-            | (Lfabsyn.LogicVar(v1,_,_), Lfabsyn.LogicVar(v2,_,_)) when (v1 = v2) ->
-                 check_args tms bndrs
-             | _ -> false)
-        in check ||
-           (List.exists (fun x -> appears_strict_tm id x bndrs) tms)
+          if (Lfabsyn.get_id_name name) = (Lfabsyn.get_id_name id)
+          then check_args tms bndrs
+          else false
+        in
+        check ||
+          (List.exists (fun x -> appears_strict_tm id x bndrs) tms)
     | Lfabsyn.ImpType(l,r,_) ->
         (appears_strict_ty id l bndrs) ||
         (appears_strict_ty id r bndrs)
     | Lfabsyn.IdType(name,_) ->
         match (name, id) with
-            (Lfabsyn.Var(v1,_,_), Lfabsyn.Var(v2,_,_)) 
-          | (Lfabsyn.LogicVar(v1,_,_), Lfabsyn.LogicVar(v2,_,_)) when (v1 = v2) ->
+            (Lfabsyn.LogicVar(v1,_,_), Lfabsyn.LogicVar(v2,_,_)) when (v1 = v2) ->
               true
           | _ ->
               false
@@ -45,17 +53,15 @@ and appears_strict_tm id tm bndrs =
         appears_strict_tm id body (name :: bndrs)
     | Lfabsyn.AppTerm(name,tms,_) ->
         let check =
-          (match (name, id) with
-               (Lfabsyn.Var(v1,_,_), Lfabsyn.Var(v2,_,_)) 
-             | (Lfabsyn.LogicVar(v1,_,_), Lfabsyn.LogicVar(v2,_,_)) when (v1 = v2) ->
-                 check_args tms bndrs
-             | _ -> false)
-        in check ||
-           (List.exists (fun x -> appears_strict_tm id x bndrs) tms)
+          if (Lfabsyn.get_id_name name) = (Lfabsyn.get_id_name id)
+          then check_args tms bndrs
+          else false
+        in
+        check ||
+          (List.exists (fun x -> appears_strict_tm id x bndrs) tms)
     | Lfabsyn.IdTerm(name,_) ->
         match (name, id) with
-             (Lfabsyn.Var(v1,_,_), Lfabsyn.Var(v2,_,_)) 
-           | (Lfabsyn.LogicVar(v1,_,_), Lfabsyn.LogicVar(v2,_,_)) when (v1 = v2) ->
+             (Lfabsyn.LogicVar(v1,_,_), Lfabsyn.LogicVar(v2,_,_)) when (v1 = v2) ->
                true
            | _ ->
              false
